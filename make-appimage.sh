585efabb33ccfb5cfe674fc17944ac5b03abd2e9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-17 22:21:11 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,23 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q webapp-manager | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/webapp-manager.svg
+export DESKTOP=/usr/share/applications/webapp-manager.desktop
+export DEPLOY_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/webapp-manager   \
+	/usr/lib/webapp-manager   \
+	/usr/share/webapp-manager \
+	/usr/lib/libgtk-3.so*     \
+	/usr/lib/libxapp.so* # is this needed?
+sed -i -e 's|/usr|"$APPDIR"|g' ./AppDir/bin/webapp-manager
 
 # Additional changes can be done in between here
 
